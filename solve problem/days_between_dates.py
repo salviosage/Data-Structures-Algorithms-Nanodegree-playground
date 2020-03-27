dbb0a8a64a63f9ec467090eea5170d8c2ef19cc2
jean salvi <salviosage@gmail.com>
2020-03-27 04:57:14 -0700

    add final solution

--- solve problem/days_between_dates.py
+++ solve problem/days_between_dates.py
@@ -10,6 +10,8 @@
 #4. simple mechanical solutions 
 # dont optimize prematurely, simple and correct 
 
+# develop incrementaly and test as you go
+
 
 
 # Credit goes to Websten from forums
@@ -23,10 +25,31 @@
 # an assertion failure when the inputs are invalid. This should
 # occur when the first date is not before the second date.
 #  
+def leapYear(year):
+    if year % 400 ==0:
+        return True 
+    
+    if year % 100 ==0:
+        return False 
+    if year % 4 ==0:
+        return True
+    return False
+    
+    
+
+def daysInMonth(year,month):
+    if month ==4 or month==6 or month==9 or month==11:
+        return 30
+    else:
+        if month ==2:
+            if leapYear(year):
+                return 29
+            return 28
+    return 31
 
 def nextDay(year, month, day):
     """Simple version: assume every month has 30 days"""
-    if day < 30:
+    if day < daysInMonth(year,month):
         return year, month, day + 1
     else:
         if month == 12:
@@ -60,6 +83,14 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     return days
 
 def test():
+    assert daysBetweenDates(2013,1,1,2013,1,1)== 0
+    assert daysBetweenDates(2013,1,1,2013,1,2)== 1
+    assert daysBetweenDates(2013,1,1,2014,1,1)== 365
+    assert nextDay(2013,1,1)== (2013,1,2)
+    assert nextDay(2013,4,30)== (2013,5,1)
+    assert nextDay(2013,12,31)== (2014,1,1)
+    assert nextDay(2013,2,28)== (2013,3,1)
+    assert nextDay(2013,9,30)== (2013,10,1)
     test_cases = [((2012,9,30,2012,10,30),30), 
                   ((2012,1,1,2013,1,1),360),
                   ((2012,9,1,2012,9,4),3),
