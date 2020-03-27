ec25a7b320a68e8b51181b5b77261b622efdc9a2
jean salvi <salviosage@gmail.com>
2020-03-27 09:55:50 -0700

    add response for task2

--- projects/unscramble comuter science problem/Task2.py
+++ projects/unscramble comuter science problem/Task2.py
@@ -2,6 +2,9 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
+from itertools import chain
+from collections import deque, defaultdict
+from datetime import datetime
 import csv
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -19,4 +22,17 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+calls_dictionary = defaultdict(int)
 
+
+for caller, reciever, timestamp, duration in calls:
+    date = datetime.strptime(timestamp, "%d-%m-%Y %H:%M:%S")
+    if date.year == 2016 and date.month == 9:
+        calls_dictionary[caller] += int(duration)
+        calls_dictionary[reciever] += int(duration)
+
+template = "{} spent the longest time, {} seconds, on the phone during September 2016."
+
+
+highest_duration = max(calls_dictionary.items(), key=lambda x: x[1])
+print(template.format(*highest_duration))
