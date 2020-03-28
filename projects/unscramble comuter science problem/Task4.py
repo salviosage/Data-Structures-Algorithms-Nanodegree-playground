2d748d8a0ac1f251b9bd10f99d8d704960219e00
jean salvi <38810299+salviosage@users.noreply.github.com>
2020-03-28 10:12:56 -0700

    Project1 (#3)
    * add problem givem data
    
    * add solutions fro challenge 0 and 1
    
    * add response for task2
    
    * add response for task3
    
    * add response for task4
    
    * add worst cases analyisis

---
+++ projects/unscramble comuter science problem/Task4.py
@@ -0,0 +1,71 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+from itertools import chain
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+
+telemarkerters = set()
+receiver_list = set()
+for call in calls:
+    receiver_list.add(call[1])
+
+for call in calls:
+    if call[0] in receiver_list:
+        pass
+    else:
+        telemarkerters.add(call[0])
+
+for text in texts:
+    if text[0] in telemarkerters:
+        telemarkerters.discard(text[0])
+    if text[1] in telemarkerters:
+        telemarkerters.discard(text[1])
+
+print("These numbers could be telemarketers: ")
+for telemarkerter in sorted(telemarkerters):
+	print(telemarkerter)
+
+
+
+
+telephone_numbers_in_texts = list(chain.from_iterable(
+    [(sender, reciever) for sender, reciever, _ in texts]))
+
+texters = set(telephone_numbers_in_texts)
+
+callers = set()
+call_recievers = set()
+
+for caller, reciever, _, _ in calls:
+    callers.add(caller)
+    call_recievers.add(reciever)
+
+# telemarkerters don't text or revicieve callers
+possible_telemarkerters = callers - (texters | call_recievers)
+
+print("These numbers could be telemarketers:")
+
+for tel_number in sorted(possible_telemarkerters):
+    print(tel_number)
